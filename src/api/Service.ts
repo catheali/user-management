f5d4ec3b19bb0c740877d478b9516c6eefd5cbf7
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-23 18:10:11 -0300

    feat: add login function to Service class

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -1,5 +1,5 @@
-import axios from "axios";
-
+import { apiUsers, apiLogin } from './api';
+import { ErrorState, LoginData } from "@/types/types"; 
 
 export class Service {
   static async getUsuarios() {
@@ -11,6 +11,23 @@ export class Service {
   static async updateUsuario(id: string, dados: any) {
   }
 
-  static async deleteUsuario(id: string) {
+  static async login(usuario: string, senha: string): Promise<{ error: ErrorState, usuario: LoginData | null }> {
+    try {
+      const response = await apiLogin.get('/');
+      const usuarios: LoginData[] = response.data;
+      const usuarioAutenticado = usuarios.find(
+        (u) => u.usuario === usuario && u.senha === senha
+      );
+
+      if (usuarioAutenticado) {
+        return { error: { valid: false, message: '' }, usuario: usuarioAutenticado }; 
+      }
+
+      return { error: { valid: true, message: 'Usuário ou senha inválidos' }, usuario: null };
+    } catch (error) {
+      console.error('Erro ao autenticar:', error);
+      return { error: { valid: true, message: 'Erro ao autenticar. Tente novamente mais tarde.' }, usuario: null };
+    }
   }
+
 }
