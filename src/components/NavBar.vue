bc4b79d6f56b5aa52cdd7acaea168c50b24f9e39
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-23 02:11:04 -0300

    feat: add navbar template

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+	<nav>
+	  <v-app-bar flat app>
+		<v-toolbar-title class="text-uppercase grey--text">
+		  <span class="font-weight-light">Gerenciamento </span>
+		  <span>de Usuarios</span>
+		</v-toolbar-title>
+		<v-spacer></v-spacer>
+	  </v-app-bar>
+  
+	</nav>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, ref, computed } from 'vue';
+  
+  export default defineComponent({
+	name: "NavBar",
+	setup() {
+	},
+  });
+  </script>
