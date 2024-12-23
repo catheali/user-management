65d3bf1128d506db36a5595f5bdcebd04ac9b3a8
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-23 18:05:03 -0300

    feat: add persistence to Pinia store

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
 import router from './router';
-import './style.css'
+import './style.css';
+import piniaPersist from 'pinia-plugin-persistedstate';
 
 const app = createApp(App);
 const pinia = createPinia();
+
+pinia
+.use(piniaPersist);
 
 app
   .use(pinia)
