85efdcd3fbc9280f051cdcbface0ecdc72bd51f0
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-23 01:10:10 -0300

    chore: configure Vuetify plugin, integrate Pinia store with Vue, update package.json

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
+import router from './router';
 import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+
+app
+  .use(pinia)
+  .use(router)
+  .use(vuetify)
+
+ app.mount('#app');
