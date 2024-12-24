8ca691e951cc29c9840884ce3b69075bf974aaa6
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-23 21:42:34 -0300

    feat: initialize usuarioStore and add getUsuarios using UserService class

--- src/stores/usuarioStore.ts
+++ src/stores/usuarioStore.ts
@@ -1,6 +1,30 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
+import { Usuario } from '@/types/models/Usuario';
+import {Service as UserService} from '@/api/Service.ts';
+import { ErrorState } from "@/types/types"; 
 
-export const useUsuarioStore = defineStore('usuario', {
+export const useUsuarioStore = defineStore('usuario', () => {
   
+	const usuariosList = ref<Usuario[]>([]);
+	const loading = ref(false);
+	const error = ref<ErrorState>({ valid: false, message: '' });
 
+	const getUsuarios = async () => {
+		loading.value = true;
+		try {
+			const usuarios = await UserService.getUsuarios();
+			usuariosList.value = usuarios
+		} catch (err) {
+			console.error('Erro ao obter usuários:', err);
+			error.value = { valid: true, message: 'Erro ao obter usuários' }
+		}
+	}
+
+	return {
+		usuariosList,
+		loading,
+		error,
+		getUsuarios
+	}
 });
