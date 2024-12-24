d05d05aa53e743fb502280433dbb1b433a91e113
Alicia Alencar <aliciacatherinealencar@gmail.com>
2024-12-24 03:21:57 -0300

    feat: add createUsuario function to Service class

--- src/api/Service.ts
+++ src/api/Service.ts
@@ -20,13 +20,36 @@ export class Service {
     }
   }
 
-  static async createUsuario(usuario: any) {
+  public static async createUsuario(usuario: { nome: string; matricula: string; idade: number; cargo: string; tipo: number }): Promise<Usuario> {
+    try {
+	  if (!usuario.nome || !usuario.matricula || !usuario.idade || !usuario.cargo || !usuario.tipo) {
+			throw new Error('Todos os campos são obrigatórios!');
+	  }
+
+	  if (usuario.idade < 18 || usuario.idade > 60) {
+		throw new Error('Idade deve estar entre 18 e 60 anos');
+	  }
+
+      const response = await apiUsers.post('/', {
+        nome: usuario.nome,
+        matricula: usuario.matricula,
+        idade: usuario.idade,
+        cargo: usuario.cargo,
+        tipo: usuario.tipo,
+      });
+      return new Usuario(
+        response.data.nome,
+        response.data.matricula,
+        response.data.idade,
+        response.data.cargo,
+        response.data.tipo
+      );
+    } catch (error) {
+      console.error('Erro ao criar usuário:', error);
+      throw new Error('Erro ao criar usuário');
+    }
   }
 
-  static async updateUsuario(id: string, dados: any) {
-  }
-
-  static async login(usuario: string, senha: string): Promise<{ error: ErrorState, usuario: LoginData | null }> {
     try {
       const response = await apiLogin.get('/');
       const usuarios: LoginData[] = response.data;
